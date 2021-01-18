c6f88d391b3880ffcab40c3f40d9224d840bcda9
federicocelico <federico.celico@gmail.com>
2021-01-18 17:30:42 -0300

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ 
+  nombre = 'Federico';
+  
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre : "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa : "19"
+    }
+
+  }
+}
